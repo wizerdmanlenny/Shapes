f6e935d0b781e9bcff941a3d109d95665680227c
wizerdmanlenny <wizerdmanlenny@gmail.com>
2023-01-02 11:19:31 -0600

    This is the first commit.

---
+++ main.cpp
@@ -0,0 +1,6 @@
+#include "main.h"
+
+int main() {
+    cout << "Hello World!" << endl;
+    return 0;
+}
---
+++ main.h
@@ -0,0 +1,8 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <iostream> /* cout, endl */
+using std::cout;
+using std::endl;
+
+#endif /* MAIN_H */
